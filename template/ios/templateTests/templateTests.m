173df1720b37f209b2e46cd77eeb9380f1eefaf4
Denis Forigo <denisforigo@gmail.com>
2020-09-25 11:56:53 -0300

    working Rocketseat advanced template

--- template/ios/templateTests/templateTests.m
+++ template/ios/templateTests/templateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface HelloWorldTests : XCTestCase
+@interface templateTests : XCTestCase
 
 @end
 
-@implementation HelloWorldTests
+@implementation templateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
